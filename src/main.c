4d34da655da5daa94778db252d389ce0f4947df1
Satyen Akolkar <s.akolkar3@gmail.com>
2016-02-11 15:31:36 -0700

    gaussian elimination completed

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int    size;
 // Function Signatures
 void gaussian_elimination();
 void jordan_elimination();
-void pivot_rows();
+int  get_max_row();
 void swap_rows();
 
 int main(int argc, char* argv[]) {
@@ -20,8 +20,14 @@ int main(int argc, char* argv[]) {
     Lab3LoadInput(&A, &size);
     x = CreateVec(size);
 
+    PrintMat(A, size, size + 1);
+
+    printf("\n");
+
     gaussian_elimination();
     jordan_elimination();
+
+    PrintMat(A, size, size + 1);
 
     DestroyVec(x);
     DestroyMat(A, size);
@@ -29,29 +35,39 @@ int main(int argc, char* argv[]) {
 }
 
 void gaussian_elimination() {
-    pivot_rows();
+    int i, j, k;
+
+    for(k = 0; k < size - 1; ++k) {
+        swap_rows(k, get_max_row(k));
+
+        for(i = k + 1; i < size; ++i) {
+            double temp = A[i][k] / A[k][k];
+
+            for(j = k; j < size + 1; ++j) {
+                A[i][j] = A[i][j] - temp * A[k][j];
+            }
+        }
+    }
 }
 
 void jordan_elimination() {
 
 }
 
-void pivot_rows() {
-    int cur_col, cur_row, max_row;
+int get_max_row(int cur_row) {
+    int cur_col = cur_row;
+    int max_row = cur_row;
 
-    for (cur_col = 0; cur_col < size; ++cur_col) {
-        cur_row = cur_col;
-        max_row = cur_row;
-
-        for( ; cur_row < size; ++cur_row) {
-            if (A[cur_row][cur_col] > A[max_row][cur_col])
-                max_row = cur_row;
-        }
-
-        swap_rows(cur_row, max_row);
+    for( ; cur_row < size; ++cur_row) {
+        if (A[cur_row][cur_col] > A[max_row][cur_col])
+            max_row = cur_row;
     }
+
+    return max_row;
 }
 
 void swap_rows(int row1, int row2) {
-    
+    double* temp = A[row1];
+    A[row1] = A[row2];
+    A[row2] = temp;
 }
